9bfe96ce685728b7e2c8433175528885a249d0bd
Saxena224pawan <saxenapawan224@gmail.com>
2021-01-02 13:09:50 +0530

    write code for creating blof in gatsby, add some blog entries using markdown

---
+++ src/pages/index.js
@@ -0,0 +1,68 @@
+import React from "react"
+import {graphql, Link } from "gatsby"
+import styled from 'styled-components'
+
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+margin-bottom:20px;
+color: blue;
+`
+
+
+export default ({data}) => (
+  <Layout>
+    <SEO title="Home" />
+    <div>
+      <h1>Pawan's Blog</h1>
+    </div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    
+ {
+    data.allMarkdownRemark.edges.map(({node} ) =>(
+      <div key={node.id}>
+ <BlogLink to ={node.fields.slug}>
+        <BlogTitle>
+        {node.frontmatter.title} - {node.frontmatter.date}
+        </BlogTitle>
+</BlogLink>
+        <p>{node.excerpt}</p>
+      </div>
+    ))
+ } 
+    <Link to="/page-2/">Go to page 2</Link> <br />
+  </Layout>
+)
+
+
+export const query = graphql`
+query {
+  allMarkdownRemark(sort:{fields:[frontmatter___date],order:DESC}) {
+    edges {
+      node {
+        id
+        frontmatter {
+          description
+          title
+          date
+        }
+        fields{
+          slug
+        }
+        excerpt
+        
+      }
+    }
+  } 
+}
+`
